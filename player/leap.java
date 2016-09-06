5e02e707c9d60631146334eb5a067eaea146632e
Aarthikanika <aarthielangovann@gmail.com>
2016-09-06 21:01:37 +0530

    Create leap.java

---
+++ player/leap.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+
+public class Alphabet {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int n1=sc.nextInt();
+		if(n1%4==0||n1%400==0||n1%100!=0)
+		{
+			System.out.println("leap year");
+		}
+		else
+		{
+		System.out.println("not a leap year");
+	}
+}
+	
+}
