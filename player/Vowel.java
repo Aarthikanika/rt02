39eba5ace6f3689e6351e8010ee9813b120af473
Aarthikanika <aarthielangovann@gmail.com>
2016-09-06 20:39:14 +0530

    Create Vowel.java

---
+++ player/Vowel.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Vowel
+{
+    public static void main(String args[])
+    {
+        char ch;
+        Scanner sc = new Scanner(System.in);
+        ch = sc.next().charAt(0);
+        if(ch=='a' || ch=='A' || ch=='e' || ch=='E' ||
+        ch=='i' || ch=='I' || ch=='o' || ch=='O' ||
+        ch=='u' || ch=='U')
+        {
+            System.out.print("Vowel");
+        }
+        else
+        {
+            System.out.print("consonant");
+        }
+    }
+}
