e32149ab232304b014199cbf9ab7e418a3c1ab85
Aarthikanika <aarthielangovann@gmail.com>
2016-09-06 21:11:09 +0530

    Create natural.java

---
+++ player/natural.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+
+
+public class Alphabet {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int n1=0;
+		if(n!=0)
+		{
+			n1=n*(n+1)/2;
+			System.out.println(n1);
+		}
+		
+	}
+}
